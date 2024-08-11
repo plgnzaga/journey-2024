17ea46612232f2e82ace0d2e912eb54c5d78d733
DIR\paul.s.t.gonzaga <paul.s.t.gonzaga@accenture.com>
2024-08-11 17:10:27 +0800

    remove toSortedrepush

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Credentials from "@/components/Credentials";
 import Home from "@/components/Home";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
 import Stories from "@/components/Stories";
-import Image from "next/image";
+
 
 export default function Main() {
   return (
